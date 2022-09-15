f6d53f5f60914a0af2a292e92d88f325a99ae872
Jordan Carmona <12204584+FeralCatColonist@users.noreply.github.com>
2022-09-15 14:39:08 -0500

    Update Find_Duplicate.js
    add summary to expression

--- Find_Duplicate.js
+++ Find_Duplicate.js
@@ -1,3 +1,4 @@
+//A calculate field expression to identify duplicates within a column and output the results to a new column
 var search_duplicate = $feature.A_FIELD_NAME_HERE
 var features = FEATURESETBYNAME($datastore, "Featureclass Name Here")
 // the @ is used to reference the variable in a way that is SQL-compliant
