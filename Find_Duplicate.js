864d6c22b31e18e293b5cce824b763bf0fb66a7b
Jordan Carmona <12204584+FeralCatColonist@users.noreply.github.com>
2022-09-16 13:51:39 -0500

    updated code comment
    add a comment about string literals

--- Find_Duplicate.js
+++ Find_Duplicate.js
@@ -5,6 +5,7 @@ var features = FEATURESETBYNAME($datastore, "Featureclass Name Here")
 var sqlExpression = "A_FIELD_NAME_HERE = @search_duplicate"
 var result = FILTER(features, sqlExpression)
 // this is an implicit return, probably not best practice
+// also, this is an example of a string literal, new in 1.11
 WHEN(
     COUNT(result) > 1, `Duplicate Value, frequency ${COUNT(result)}`,
     "Unique"
